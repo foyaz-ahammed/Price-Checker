1361d10a642c16daf74584f06e4ff766c411be10
kouthao <kouthao0916@gmail.com>
2021-09-11 01:01:07 -0700

    initial commit

---
+++ app/src/main/java/com/Caper/priceapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.Caper.priceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
